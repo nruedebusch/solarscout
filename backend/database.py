bc9d8dd979f5daecee5f54029679d16ac8898664
Niklas <n.ruedebusch@web.de>
2026-02-12 21:41:45 +0100

    ssl import fix

--- backend/database.py
+++ backend/database.py
@@ -41,7 +41,6 @@ class Database:
         if self._pool is not None:
             return
         
-            import ssl
         ssl_ctx = ssl.create_default_context()
         ssl_ctx.check_hostname = False
         ssl_ctx.verify_mode = ssl.CERT_NONE
